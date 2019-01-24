142e91397dbedf01351fefc5e39b14f242261079
Andranique Boone <andranique.boone@gmail.com>
2019-01-24 03:05:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  array = ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array << "five"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift("zero")
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop("four")
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift("zero")
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[0]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.last
 end
